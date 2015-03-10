a89596710bf4f9a408df296b7a7346648e2e5aa0
Eric Kuhn <erickuhn19@gmail.com>
2015-03-09 22:13:44 -0400

    Add testing for email format

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Eric Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match /1 z Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Eric order shipped.", mail.subject
+    assert_equal ["dave@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match /<td>1&times;<\/td>/, mail.body.encoded
   end
 
 end
