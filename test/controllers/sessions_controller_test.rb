788bbde18460f52c472c49e05a4de58828032662
Eric Kuhn <erickuhn19@gmail.com>
2015-03-14 11:31:34 -0400

    Typo in sessions controller test commit

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,19 +9,19 @@ class SessionsControllerTest < ActionController::TestCase
   test "should login" do
     dave = users(:one)
     post :create, name: dave.name, password: 'secret'
-    assert_redirecred_to admin_url
+    assert_redirected_to admin_url
     assert_equal dave.id, session[:user_id]
   end
 
   test "should fail login" do
     dave = users(:one)
     post :create, name: dave.name, password: 'wrong'
-    assert_redirecred_to login_url
+    assert_redirected_to login_url
   end
 
   test "should logout" do
     delete :destroy 
-    assert_redirecred_to store_url
+    assert_redirected_to store_url
   end
 
 end
